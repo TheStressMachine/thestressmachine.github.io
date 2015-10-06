94ac462550b23ecf40683b56ad4e41aac55aa9f8
TheStressMachine <mwthomas@gmail.com>
2015-10-06 15:21:05 -0400

    resetting

---
+++ set.bat
@@ -0,0 +1,7 @@
+echo # thestressmachine.github.io >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/TheStressMachine/thestressmachine.github.io.git
+git push -u origin master
+pause
